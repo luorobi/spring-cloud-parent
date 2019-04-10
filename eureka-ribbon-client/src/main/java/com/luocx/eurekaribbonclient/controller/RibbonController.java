4a3fcdd434ecf40290cd18c3fddfd732fd684b56
luo.robi <9332223@qq.com>
2019-04-10 10:29:59 +0800

    loadBalancer测试

--- eureka-ribbon-client/src/main/java/com/luocx/eurekaribbonclient/controller/RibbonController.java
+++ eureka-ribbon-client/src/main/java/com/luocx/eurekaribbonclient/controller/RibbonController.java
@@ -2,6 +2,9 @@ package com.luocx.eurekaribbonclient.controller;
 
 import com.luocx.eurekaribbonclient.service.RibbonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,8 +19,19 @@ public class RibbonController {
     @Autowired
     RibbonService ribbonService;
 
+    @Autowired
+    private LoadBalancerClient loadBalancer;
+
     @GetMapping("/hi")
-    public String hi(@RequestParam(required = false, defaultValue = "luocx") String name){
+    public String hi(@RequestParam(required = false, defaultValue = "luocx") String name) {
         return ribbonService.hi(name);
+    }
+
+
+
+    @GetMapping("/testRibbon")
+    public String testRibbon() {
+        ServiceInstance instance = loadBalancer.choose("eureka-client");
+        return instance.getHost() + ":" + instance.getPort();
     }
 }
